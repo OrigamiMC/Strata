71ce68250a8dcea67e21989c76e885fb8bcc00df
Oliver <oschi1303@gmail.com>
2026-03-23 11:38:25 +0100

    Update version to 1.0.1

--- test-env/build.gradle.kts
+++ test-env/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("java")
     id("maven-publish")
     id("com.gradleup.shadow")
-    id("com.origamimc.strata-workspace") version "1.0.0-LOCAL-SNAPSHOT"
+    id("com.origamimc.strata-workspace") version "1.0.1"
 }
 
 java {
