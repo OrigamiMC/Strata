0a24ac0a3402b18260ef806267ebbc58d467c81f
Oliver <oschi1303@gmail.com>
2026-04-07 16:46:33 +0200

    mc-diff: Update to 26.2-snapshot-1

--- strata-workspace/src/main/java/com/origamimc/strata/cli/Main.java
+++ strata-workspace/src/main/java/com/origamimc/strata/cli/Main.java
@@ -18,7 +18,7 @@ public class Main {
         // strata.getWorkspaceService().initGitDirectory("mc-diff");
 
         // Fetch version
-        String version = "26.1.1";
+        String version = "26.2-snapshot-1";
 
         PistonVersionDetails ver = strata.getMojangService().getVersion(version);
         strata.getMojangService().downloadServerBundle(ver);
